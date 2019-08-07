04f8d512f3b5f27756b7cda9c4254108be7e976b
周豪峰 <hfzhou@leqee.com>
2019-08-07 11:33:24 +0800

    JSONUtil bug修改

--- src/main/java/com/leqee/demo/controller/JSONController.java
+++ src/main/java/com/leqee/demo/controller/JSONController.java
@@ -1,5 +1,6 @@
 package com.leqee.demo.controller;
 
+import com.alibaba.fastjson.JSONArray;
 import com.alibaba.fastjson.JSONObject;
 import com.leqee.demo.utils.JSONUtil;
 import lombok.extern.log4j.Log4j2;
@@ -15,8 +16,22 @@ import java.util.Map;
 public class JSONController {
 
 
-    @RequestMapping(value = "/parse/json")
+    @RequestMapping(value = "/parse/json2")
     public Object getFile(@RequestParam("a") String var1, @RequestParam("b") String var2) {
+       /* List list = JSONUtil.findAllObjectRecursive("name", jsonObject, List.class);
+        return list;*/
+        return "aaa";
+    }
+
+    @RequestMapping(value = "/parse/json")
+    public Object getFile(@RequestBody JSONObject jsonObject) {
+       /* List list = JSONUtil.findAllObjectRecursive("name", jsonObject, List.class);
+        return list;*/
+        return "aaa";
+    }
+
+    @RequestMapping(value = "/parse/array")
+    public Object getFile(@RequestBody JSONArray jsonArray) {
        /* List list = JSONUtil.findAllObjectRecursive("name", jsonObject, List.class);
         return list;*/
         return "aaa";
--- src/main/java/com/leqee/demo/utils/JSONUtil.java
+++ src/main/java/com/leqee/demo/utils/JSONUtil.java
@@ -7,7 +7,17 @@ import java.util.Map;
 public abstract class JSONUtil {
 
 
+    /**
+     * 递归寻找list中的对象
+     *
+     * @param key    可为null,为null则在list中寻找非key-value形式的符合条件的值
+     * @param list   被寻找的list集合
+     * @param tClass 寻找的对象类型(支持8中基本数据类型)(不支持数组类型)
+     * @param <T>
+     * @return
+     */
     public static <T> List<T> findAllObjectRecursiveFromList(String key, List list, Class<T> tClass) {
+        if (list == null) throw new IllegalArgumentException("list can't be null");
         if (tClass == null) throw new IllegalArgumentException("tClass can't be null");
         //不支持数组类型
         if (tClass.getName().startsWith("[")) throw new UnsupportedOperationException("Array object is not supported");
@@ -18,7 +28,7 @@ public abstract class JSONUtil {
 
     private static <T> void findAllObjectRecursiveFromList(String key, List list, List resultList, Class<T> tClass) {
         for (Object obj : list) {
-            if (tClass.isAssignableFrom(obj.getClass())) {
+            if (key == null && tClass.isAssignableFrom(obj.getClass())) {
                 resultList.add(obj);
             }
             if (List.class.isAssignableFrom(obj.getClass())) {
@@ -33,7 +43,17 @@ public abstract class JSONUtil {
         }
     }
 
+    /**
+     * 递归寻找map中的对象
+     *
+     * @param key    可为null,为null则在list中寻找非key-value形式的符合条件的值
+     * @param map    被寻找的map集合
+     * @param tClass 寻找的对象类型(支持8中基本数据类型)(不支持数组类型)
+     * @param <T>
+     * @return
+     */
     public static <T> List<T> findAllObjectRecursive(String key, Map<String, Object> map, Class<T> tClass) {
+        if (map == null) throw new IllegalArgumentException("map can't be null");
         if (tClass == null) throw new IllegalArgumentException("tClass can't be null");
         //不支持数组类型
         if (tClass.getName().startsWith("[")) throw new UnsupportedOperationException("Array object is not supported");
@@ -60,26 +80,43 @@ public abstract class JSONUtil {
         }
     }
 
-
+    /**
+     * 从List中寻找符合条件的对象(一旦找到一个符合条件的就立刻停止)
+     *
+     * @param key    可为null,为null则在list中寻找非key-value形式的符合条件的值,否则寻找key-value形式的value
+     * @param list   被寻找的list
+     * @param tClass 目标类型
+     * @param <T>
+     * @return
+     */
     public static <T> T findObjectRecursiveFromList(String key, List list, Class<T> tClass) {
-        if (list == null || list.size() == 0) return null;
+        if (list == null) throw new IllegalArgumentException("list can't be null");
+        if (list.size() == 0) return null;
         for (Object obj : list) {
-            Object result = null;
-            if (tClass.isAssignableFrom(obj.getClass())) {
-                result = obj;
+            if (key == null && tClass.isAssignableFrom(obj.getClass())) {
+                return (T) obj;
             }
             if (List.class.isAssignableFrom(obj.getClass())) {
-                result = findObjectRecursiveFromList(key, (List) obj, tClass);
+                return findObjectRecursiveFromList(key, (List) obj, tClass);
             }
             if (Map.class.isAssignableFrom(obj.getClass())) {
-                result = findObjectRecursive(key, (Map<String, Object>) obj, tClass);
+                return findObjectRecursive(key, (Map<String, Object>) obj, tClass);
             }
-            if (result != null) return (T) result;
         }
         return null;
     }
 
+    /**
+     * 从Map中寻找符合条件的对象(一旦找到一个符合条件的就立刻停止)
+     *
+     * @param key    可为null,为null则在list中寻找非key-value形式的符合条件的值,否则寻找key-value形式的value
+     * @param map    被寻找的Map
+     * @param tClass 目标类型
+     * @param <T>
+     * @return
+     */
     public static <T> T findObjectRecursive(String key, Map<String, Object> map, Class<T> tClass) {
+        if (map == null) throw new IllegalArgumentException("map can't be null");
         if (tClass == null) throw new IllegalArgumentException("tClass can't be null");
         //不支持数组类型的检索
         if (tClass.getName().startsWith("["))
@@ -88,9 +125,10 @@ public abstract class JSONUtil {
         return findObjectRecursive0(key, map, tClass);
     }
 
+
     private static <T> T findObjectRecursive0(String key, Map<String, Object> map, Class<T> tClass) {
         Object obj = map.get(key);
-        if (obj != null && obj.getClass().equals(tClass)) {
+        if (obj != null && tClass.isAssignableFrom(obj.getClass()) && key != null) {
             return (T) obj;
         }
         for (Map.Entry e : map.entrySet()) {
@@ -101,12 +139,17 @@ public abstract class JSONUtil {
                 if (result == null) continue;
                 else return (T) result;
             }
+            if (eValue instanceof List) {
+                Object result = findObjectRecursiveFromList(key, (List) eValue, tClass);
+                if (result == null) continue;
+                else return (T) result;
+            }
         }
         //not found
         return null;
     }
 
-    public static <T> Class<T> adaptPrimitive(Class<T> clazz) {
+    private static <T> Class<T> adaptPrimitive(Class<T> clazz) {
         if (clazz.isPrimitive()) {
             switch (clazz.getName()) {
                 case "byte":
