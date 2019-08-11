33f9481da7b74215e7aaa8217f3675be3fa0e516
zhou <1014347942@qq.com>
2019-08-11 21:37:16 +0800

    test

--- src/main/java/com/leqee/demo/netty/Test.java
+++ src/main/java/com/leqee/demo/netty/Test.java
@@ -2,13 +2,10 @@ package com.leqee.demo.netty;
 
 import org.springframework.util.ReflectionUtils;
 
-import java.io.File;
 import java.lang.reflect.Field;
 import java.text.ParseException;
-import java.text.SimpleDateFormat;
-import java.util.Date;
 import java.util.Random;
-import java.util.UUID;
+import java.util.concurrent.FutureTask;
 
 public class Test {
 
@@ -21,6 +18,18 @@ public class Test {
         inner.inn = "b";
         System.out.println(inner.inn);
         System.out.println(new Test().new Inner().inn);
+        String a = null;
+        Runnable runnable = new Runnable() {
+            @Override
+            public void run() {
+                System.out.println(a);
+            }
+        };
+        System.out.println();
+    }
+
+    protected FutureTask test1(){
+        return null;
     }
 
     public class Inner {
@@ -30,16 +39,26 @@ public class Test {
             System.out.println(test);
         }
     }
-        //1564052213060
-        //16732 80600 000
+
+    public static class TestInner extends Test{
+        @Override
+         public FutureTask test1() {
+            return null;
+        }
+    }
+
+    //1564052213060
+    //16732 80600 000
     public static void main(String[] args) throws ParseException, NoSuchFieldException {
 //        System.out.println(UUID.randomUUID().toString());
         /*SimpleDateFormat sdf = new SimpleDateFormat("yyyy-mm-dd");
         Date date = sdf.parse("2023-10-10");
         System.out.println(date.getTime());*/
         Test t = new Test();
+        t.test();
+        t.new Inner().test();
         Field f = Test.class.getField("test1");
-        ReflectionUtils.setField(f,t,null);
+        ReflectionUtils.setField(f, t, null);
 
         Random random = new Random();
         for (int i = 0; i < 20; i++) {
